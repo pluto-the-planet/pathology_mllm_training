f181c5e4adb591eac91d3d0e7cd65c0148dcfdb1
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-07-08 12:46:56 +0500

    Update train_mem.py

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -3,7 +3,7 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
 # Need to call this before importing transformers.
-from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+from kaggle.working.pathology_mllm_training.llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
 
