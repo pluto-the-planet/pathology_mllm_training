46e65688575727a6510128aebcabf8b69911b54f
pluto-the-planet <86825641+pluto-the-planet@users.noreply.github.com>
2024-07-09 15:05:52 +0500

    Update train.py

--- llava/train/train.py
+++ llava/train/train.py
@@ -26,7 +26,7 @@ from typing import Dict, Optional, Sequence, List
 import torch
 
 import transformers
-from llava.constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from torch.utils.data import Dataset
 from llava_trainer import LLaVATrainer
 
